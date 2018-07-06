53aa4894e1a3173493384f90eea8300cc6483e16
nash-md <mdamm@twilio.com>
2018-07-06 15:11:51 +0200

    refactored token generation + prettier

--- connector.js
+++ connector.js
@@ -8,14 +8,13 @@ const privateKey = `-----BEGIN RSA PRIVATE KEY-----
 {{private-key}}
 -----END RSA PRIVATE KEY-----`;
 
-exports.handler = function (context, event, callback) {
-
+function getTokenInformation() {
   const options = {
     issuer: clientId,
     audience: 'https://login.salesforce.com',
     expiresIn: 180,
     algorithm: 'RS256'
-  }
+  };
 
   const token = jwt.sign({ prn: user }, privateKey, options);
 
@@ -24,40 +23,47 @@ exports.handler = function (context, event, callback) {
   form.append('grant_type', 'urn:ietf:params:oauth:grant-type:jwt-bearer');
   form.append('assertion', token);
 
-  got.post(`https://login.salesforce.com/services/oauth2/token`, {
-    body: form
-  }).then((response) => {
-
-    const token = JSON.parse(response.body);
-
-    const options = {
-      Authorization: 'Bearer ' + token.access_token,
-      'Content-Type': 'application/json'
-    };
-
-    return got(`${token.instance_url}/services/data/v41.0/query/`, {
-      headers: options,
-      query: 'q=' + encodeURIComponent(`SELECT Id, Name from Contact WHERE Phone = '${event.phoneNumber}'`)
+  return got
+    .post(`https://login.salesforce.com/services/oauth2/token`, {
+      body: form
     })
+    .then(response => {
+      const token = JSON.parse(response.body);
+      return token;
+    });
+}
 
-  }).then(response => {
-    const attributes = JSON.parse(response.body);
-
-    console.log(`found: ${attributes.records.length} records`);
-
-    if (attributes.records.length !== 0) {
-
-      callback(null, {
-        name: attributes.records[0].Name
+exports.handler = function(context, event, callback) {
+  getTokenInformation()
+    .then(token => {
+      const options = {
+        Authorization: 'Bearer ' + token.access_token,
+        'Content-Type': 'application/json'
+      };
+      return got(`${token.instance_url}/services/data/v41.0/query/`, {
+        headers: options,
+        query:
+          'q=' +
+          encodeURIComponent(
+            `SELECT Id, Name from Contact WHERE Phone = '${event.phoneNumber}'`
+          )
       });
+    })
+    .then(response => {
+      const attributes = JSON.parse(response.body);
 
-    } else {
-      callback(null, {});
-    }
+      console.log(`found: ${attributes.records.length} records`);
 
-  }).catch(error => {
-    console.error(error);
-    callback(error);
-  })
-
+      if (attributes.records.length !== 0) {
+        callback(null, {
+          name: attributes.records[0].Name
+        });
+      } else {
+        callback(null, {});
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      callback(error);
+    });
 };
